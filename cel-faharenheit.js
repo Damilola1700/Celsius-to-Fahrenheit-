6320e3f506e44dab8d52ea7b8d9c5d647b04594f
loveth <lovethdamilola206@gmail.com>
2026-05-10 17:51:52 +0100

    celsius to fahrenheit

---
+++ cel-faharenheit.js
@@ -0,0 +1,29 @@
+
+
+function toFahrenheit (a){
+    let toFahrenheit = (a * 9/5) + 32;
+    return toFahrenheit;
+}
+
+let form = document.querySelector("#myForm"); 
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let userInput = form ["inputCelcius"].value;
+  let paragraph = document.querySelector("#pWords");
+  let result = toFahrenheit(userInput) 
+  
+  
+  
+  if (userInput === "" ){
+    paragraph.innerHTML = "Input your Number";
+  } else {
+    let result = toFahrenheit(userInput);
+    paragraph.innerHTML = `conversion of ${userInput} celcius to Fahrenheit is ${result}`
+  }
+})
+
+
+
+
